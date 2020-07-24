360a936d759fbfb44b45a70882245ddb4b4cef54
Prince of Kochi <mathenjs@gmail.com>
2020-07-23 21:42:50 -0700

    changed controller for heroku testing purposes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,13 +6,13 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-// router.get("/", function(req, res) {
+router.get("/", function(req, res) {
 //     cat.all(function(data) {
 //       var hbsObject = {
 //         cats: data
 //       };
 //       console.log(hbsObject);
-//       res.render("index", hbsObject);
+      res.render("index");
 //     });
 //   });
 
